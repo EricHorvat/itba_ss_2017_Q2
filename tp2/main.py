6c8cd122f6d6029fcf5d97ae712b24591726663f
Eric Horvat <eric.nahuel.horvat@gmail.com>
2017-08-27 18:16:16 -0300

    [ADD] Some minor fixes

--- tp2/main.py
+++ tp2/main.py
@@ -39,14 +39,14 @@ def usage():
 	print('usage:	python main.py [options]')
 	print('-h --help:	print this screen')
 	print('-c --config=:	configuration file')
-	print('-b --border:	The distance is calculated if the border are traspasable')
+	print('-p --partition=:	Va value file will be generated for each iterations')
 
 def parse_arguments():
 	arguments = {
 		'config': './config/config.json',
 	}
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], 'hc:tn', ['help', 'config=', 'border'])
+		opts, args = getopt.getopt(sys.argv[1:], 'hc:tn', ['help', 'config=', 'partition='])
 	
 	except getopt.GetoptError as err:
 		print str(err)  # will print something like "option -a not recognized"
@@ -59,8 +59,8 @@ def parse_arguments():
 			sys.exit()
 		elif o in ('-c', '--config'):
 			arguments['config'] = a
-		elif o in ('-b','--border'):
-			arguments['border_control'] = True
+		elif o in ('-p','--partition'):
+			arguments['partition'] = int(a)
 		else:
 			assert False, 'unknown option `' + o + '`' 
 
@@ -134,21 +134,21 @@ def get_field(M, L, in_particles = []):
 		y = in_particles[index]["y"]
 		vx = in_particles[index]["vx"]
 		vy = in_particles[index]["vy"]
-		#TODO ADD TO FIELD
-		field[int(floor(x * M / L))][int(floor(y * M / L))].append({"part": index, "x": x, "y": y, "vx": vx, "vy": vy, "x_c": int(floor(x * M / L)), "y_c": int(floor(y * M / L))})
+		r = in_particles[index]["r"]
+		field[int(floor(x * M / L))][int(floor(y * M / L))].append({"part": index, "x": x, "y": y, "r": r, "vx": vx, "vy": vy, "x_c": int(floor(x * M / L)), "y_c": int(floor(y * M / L))})
 
 	return field
 
-def generate(N, L):
+def generate(N, L, r, v):
 	particles = []
 	for part in xrange(1, N + 1):
 		x = random.random() * (L)
 		y = random.random() * (L)
 		angle = random.random() * 2 * pi
-		vx = cos(angle) * 0.03
-		vy = sin(angle) * 0.03
+		vx = cos(angle) * v
+		vy = sin(angle) * v
 		color = get_color(angle)
-		particles.append({ "x": x, "y": y, "vx": vx, "vy": vy, "color":color})
+		particles.append({ "part": part, "x": x, "y": y, "vx": vx, "vy": vy, "r": r, "color":color})
 	return particles
 
 def get_info(particles, i):
@@ -156,18 +156,26 @@ def get_info(particles, i):
 	string += '\t' + str(len(particles)) + '\n'
 	string += '\t' + str(i) + '\n'
 	for particle in particles:
-		string += '\t' + str(particle["x"]) + '\t' + str(particle["y"]) + '\t' + str(particle["vx"]) + '\t' + str(particle["vy"]) + '\t' + str(0.25) + '\t' + str(particle["color"]["R"]) + '\t' + str(particle["color"]["G"]) + '\t' + str(particle["color"]["B"]) + '\n'
+		string += '\t' + str(particle["x"]) + '\t' + str(particle["y"]) + '\t' + str(particle["vx"]) + '\t' + str(particle["vy"]) + '\t' + str(particle["r"]) + '\t' + str(particle["color"]["R"]) + '\t' + str(particle["color"]["G"]) + '\t' + str(particle["color"]["B"]) + '\n'
 	return string
 
-def save_file(file_string):
-	with open('output/aneighbours.txt', 'w') as outfile:
-		outfile.write(file_string)
+VA = "va"
+FILE = "file"
 
-def start(M , L, particles, rc, eta):
+def save_file(typee,file_string):
+	if typee == VA:
+		with open('output/va.txt', 'w') as outfile:
+			outfile.write(file_string)
+	elif typee == FILE:
+		with open('output/neighbours.txt', 'w') as outfile:
+			outfile.write(file_string)
+
+def start(M , L, particles, rc, eta, partition, v, iterations):
 	i = 0
 
 	file_string = ""
-	while i < 1000:
+	va_string = ""
+	while i < iterations:
 		print i
 		field = get_field(M = M, L = L, in_particles = copy.deepcopy(particles))
 
@@ -179,6 +187,8 @@ def start(M , L, particles, rc, eta):
 
 		x_sum = 0
 		y_sum = 0
+		vx_sum = 0
+		vy_sum = 0
 
 		for (index,particle) in enumerate(particles):
 			particle["x"]= (particle["x"] + particle["vx"] ) % L # dt = 1
@@ -186,24 +196,31 @@ def start(M , L, particles, rc, eta):
 			x_sum += particle["x"]
 			y_sum += particle["y"]
 			particle_neighbours = neighbours[index][1]
-			sum_speed = reduce(lambda speed, neighbour: (speed[0] + neighbour["vy"]/0.03, speed[1] + neighbour["vx"]/0.03), particle_neighbours, (0,0))
+			sum_speed = reduce(lambda speed, neighbour: (speed[0] + neighbour["vy"]/v, speed[1] + neighbour["vx"]/v), particle_neighbours, (0,0))
 			angle = (numpy.random.uniform(-eta/2,eta/2) +
 					atan2(sum_speed[0]/len(particle_neighbours),sum_speed[1]/len(particle_neighbours)) )
 			particle["vx"]= cos(angle)
 			particle["vy"]= sin(angle)
+			vx_sum += cos(angle) 
+			vy_sum += sin(angle)
 			particle["color"]= get_color(angle)
 
-		va = sqrt(x_sum**2 + y_sum**2) / (len(particles) *0.03)
+		if partition > 0 and i%partition == 0:
+			va = sqrt(vx_sum**2 + vy_sum**2) / (len(particles))
+			va_string += str(va).replace(".",",") + '\n'
 		i+=1
 
 
 	file_string += get_info(particles,i)
-	save_file(file_string)
+	save_file(FILE,file_string)
+	save_file(VA,va_string) if va_string != "" else (lambda x: x ,[])
 
 
 def main():
 
 	arguments = parse_arguments()
+
+	partition = arguments['partition'] if 'partition' in arguments else 0
 	
 	init()
 
@@ -216,13 +233,16 @@ def main():
 	M = data['world']["M"] if 'world' in data else data["M"] 
 	L = data['world']["L"] if 'world' in data else data["L"]
 	rc = data['world']["rc"] if 'world' in data else data["rc"]
+	iterations = data['world']["interations"] if 'world' in data else data["iterations"]
+	r = data['world']["r"] if 'world' in data else data["r"]
+	v = data['world']["v"] if 'world' in data else data["v"]
 	N = data['world']["N"] if 'world' in data else data["N"]
 	eta = data['world']["eta"] if 'world' in data else data["eta"]
-	particles = data['particles'] if 'particles' in data else generate(N = N, L = L)
+	particles = data['particles'] if 'particles' in data else generate(N = N, L = L, r = r, v = v)
 
 	density = N/(L**2)
 
-	start(M = M, L = L, rc = rc, particles = particles, eta = eta)	
+	start(M = M, L = L, rc = rc, particles = particles, eta = eta, partition = partition, v = v, iterations = iterations)	
 
 
 if __name__ == '__main__':
