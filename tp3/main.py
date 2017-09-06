e0b863ed7d1d572d9bb37bb133f83c2df69a1b39
Eric Horvat <eric.nahuel.horvat@gmail.com>
2017-09-06 17:22:42 -0300

    [ADD] Last implementation without cell index

--- tp3/main.py
+++ tp3/main.py
@@ -63,7 +63,7 @@ def parse_arguments():
 			arguments['partition'] = int(a)
 		elif o in ('--eta_step'):
 			arguments['eta_step'] = int(a)
-		elif o in ('--density_step'):
+		elif o in ('--density_stec'):
 			arguments['density_step'] = int(a)
 		else:
 			assert False, 'unknown option `' + o + '`' 
@@ -83,11 +83,17 @@ class Particle(object):
 		self.m = m
 		self.color = color
 
+	def __str__(self):
+		return "X = " + str(self.x) + ", Y = " + str(self.y) + ", Vx = " + str(self.vx) + ", Vy = " + str(self.vy)
+
+	def __repr__(self):
+		return "X = " + str(self.x) + ", Y = " + str(self.y) + ", Vx = " + str(self.vx) + ", Vy = " + str(self.vy)
+
 def generate(N, L, r, v, m, r2, m2):
 	
 	x = random.random() * (L - 2 * r2) + r2
 	y = random.random() * (L - 2 * r2) + r2
-	particles = [Particle(x = x, y = y, vx = 0, vy = 0, r = r2, m = m2, color = "R")]
+	particles = [Particle(x = x, y = y, vx = 0, vy = 0, r = r2, m = m2, color = "0")]
 	for part in xrange(1, N + 1):
 		print part
 		x = random.random() * (L - 2 * r) + r
@@ -97,11 +103,11 @@ def generate(N, L, r, v, m, r2, m2):
 		vx = cos(angle) * v_rand
 		vy = sin(angle) * v_rand
 		while reduce(lambda acumm, elem: (hypot(elem.x-x, elem.y-y) < r + elem.r) or acumm, particles, False):
-			x = random.random() * L
-			y = random.random() * L	
+			x = random.random() * (L - 2 * r) + r
+			y = random.random() * (L - 2 * r) + r
 
 		print x, y
-		particles.append(Particle(x = x, y = y, vx = vx, vy = vy, r = r, m = m, color = "B"))
+		particles.append(Particle(x = x, y = y, vx = vx, vy = vy, r = r, m = m, color = "1"))
 	return particles
 
 def get_info(particles, L, i):
@@ -109,21 +115,25 @@ def get_info(particles, L, i):
 	string += '\t' + str(len(particles)+4) + '\n'
 	string += '\t' + str(i) + '\n'
 	for particle in particles:
-		string += '\t' + str(particle.x) + '\t' + str(particle.y) + '\t' + str(particle.r) + '\n'
-	string += '\t' + str(0) + '\t' + str(0) + '\t' + str(0.00000001) + '\n'
-	string += '\t' + str(L) + '\t' + str(0) + '\t' + str(0.00000001) + '\n'
-	string += '\t' + str(0) + '\t' + str(L) + '\t' + str(0.00000001) + '\n'
-	string += '\t' + str(L) + '\t' + str(L) + '\t' + str(0.00000001) + '\n'
+		string += '\t' + str(particle.x) + '\t' + str(particle.y) + '\t' + str(particle.r) + '\t' + str(particle.color) + '\n'
+	string += '\t' + str(0) + '\t' + str(0) + '\t' + str(0.00000001) + '\t' + '0' + '\n'
+	string += '\t' + str(L) + '\t' + str(0) + '\t' + str(0.00000001) + '\t' + '0' + '\n'
+	string += '\t' + str(0) + '\t' + str(L) + '\t' + str(0.00000001) + '\t' + '0' + '\n'
+	string += '\t' + str(L) + '\t' + str(L) + '\t' + str(0.00000001) + '\t' + '0' + '\n'
 	return string
 
 VA = "va"
 FILE = "file"
 
-def save_file(file_string):
-	with open('output/brownian.txt', 'w') as outfile:
+def save_file(file_string, i = False):
+	with open('output/brownian' + (str(i) if i else '') + '.txt', 'w') as outfile:
 		outfile.write(file_string)
 
-def start(L, particles, iterations):
+def save_data(file_string):
+	with open('output/data.txt', 'w') as outfile:
+		outfile.write(file_string)
+
+def start(L, particles, iterations, dt):
 	i = 0
 
 	x1 = 0
@@ -132,11 +142,25 @@ def start(L, particles, iterations):
 	y2 = L
 
 	file_string = get_info(particles = particles,L = L, i = i)
+	big_particle = particles[0]
+	t_count = 0
+	t_sum = 0
+	v_sum = 0
 	import ipdb
-	while i < iterations:
-		print i
-		#ipdb.set_trace()
-	
+	while not ( (big_particle.r - big_particle.x) == x1 or \
+		(big_particle.r + big_particle.x) == x2 or \
+		(big_particle.r - big_particle.y) == y1 or \
+		(big_particle.r + big_particle.y) == y2):
+
+		if i % 100 == 0:
+			print i
+			print big_particle
+			if i % 1000 == 0 and i != 0:
+				save_file(file_string = file_string, i=i)
+				save_data("v_data = "+str(v_sum/i)+", t_data ="+ str(t_sum/i))
+			elif i == 0:
+				save_file(file_string = file_string, i=i)
+			#ipdb.set_trace()
 
 		min_tc = False
 		# 2
@@ -157,6 +181,9 @@ def start(L, particles, iterations):
 			
 			min_tc = (x_tc,index,-1) if not min_tc or x_tc <= min_tc[0] else min_tc
 			min_tc = (y_tc,index,-2) if y_tc <= min_tc[0] else min_tc
+			if min_tc[0] < 0:
+				ipdb.set_trace()
+
 
 			for (other_index, other_particle) in enumerate(particles):
 				if index < other_index:
@@ -171,12 +198,16 @@ def start(L, particles, iterations):
 					d = dvdr**2 - dvdv * (drdr - sigma**2)
 					if d >=0 and dvdr < 0:
 						tc =  - (dvdr + sqrt(d)) / dvdv
-						min_tc = (tc,index,other_index) if tc <= min_tc[0] else min_tc
-			print min_tc
+						if tc < 0:
+							ipdb.set_trace()
 
+						min_tc = (tc,index,other_index) if tc <= min_tc[0] else min_tc
+			
 		#ipdb.set_trace()
 
 		tc = min_tc[0]
+		if tc < 0:
+			ipdb.set_trace()
 		collide_particle = particles[min_tc[1]]
 		collide_element_index = min_tc[2]
 		
@@ -206,12 +237,21 @@ def start(L, particles, iterations):
 			collide_particle.vy = collide_particle.vy + Jy /collide_particle.m
 			collide_other_particle.vx = collide_other_particle.vx - Jx /collide_other_particle.m
 			collide_other_particle.vy = collide_other_particle.vy - Jy /collide_other_particle.m
-		file_string += get_info(particles = particles,L = L, i = i)
+		
+		t_count += min_tc[0]
+		t_sum += min_tc[0]
+		v_data = reduce(lambda acumm, elem: acumm + hypot(elem.vx,elem.vy),particles,0) / len(particles)
+		v_sum += v_data 
+		if t_count > dt:
+			file_string += get_info(particles = particles,L = L, i = i)
+			t_count = 0
 
 		i+=1
 
 	save_file(file_string = file_string)
-	
+	save_data("v_data = "+str(v_sum/i)+", t_data ="+ str(t_sum/i))
+	print t_sum	
+	#TOD GET RC FROM 2 * TC PROM * VC
 
 def main():
 
@@ -231,6 +271,7 @@ def main():
 
 	iterations = data['world']["interations"] if 'world' in data else data["iterations"]
 	N = data['world']["N"] if 'world' in data else data["N"]
+	dt = data['world']["dt"] if 'world' in data else data["dt"]
 
 	L=0.5
 	r=0.005
@@ -253,7 +294,7 @@ def main():
 			particles = generate(N = N_step, L = L, r = r, v = v)
 			start(M = M, L = L, rc = rc, particles = particles, eta = eta, partition = partition, v = v, iterations = iterations, parameter="particle_number")
 	else:
-			start(L = L, particles = particles, iterations = iterations)
+			start(L = L, particles = particles, iterations = iterations, dt = dt)
 
 
 
